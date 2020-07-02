7d038a1da1c05ac67db4f61d094facb1842c8abf
jickingx <jickingx@gmail.com>
2020-07-02 14:15:39 +0800

    added player

---
+++ src/Actors/Actor.gd
@@ -0,0 +1,5 @@
+extends KinematicBody2D
+class_name Actor
+
+const TARGET_FPS: = 60
+const FLOOR_NORMAL: = Vector2.UP
---
+++ src/Actors/Player.gd
@@ -0,0 +1,43 @@
+extends Actor
+
+
+const ACCELERATION: = 64
+const MAX_SPEED: = 384
+const FRICTION: = 10
+const AIR_RESISTANCE: = 1
+const GRAVITY: = 10
+const JUMP_FORCE: = 384
+
+var motion: = Vector2.ZERO
+
+onready var animatedSprite = $AnimatedSprite
+
+func _physics_process(delta):
+	var x_input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if x_input != 0:
+		animatedSprite.play("walk")
+		motion.x += x_input * ACCELERATION * delta * TARGET_FPS
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		animatedSprite.flip_h = x_input < 0
+	else:
+		animatedSprite.play("idle")
+	
+	motion.y += GRAVITY * delta * TARGET_FPS
+	
+	if is_on_floor():
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, FRICTION * delta)
+			
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -JUMP_FORCE
+	else:
+		animatedSprite.play("walk")
+		
+		if Input.is_action_just_released("ui_up") and motion.y < -JUMP_FORCE/2:
+			motion.y = -JUMP_FORCE/2
+		
+		if x_input == 0:
+			motion.x = lerp(motion.x, 0, AIR_RESISTANCE * delta)
+	
+	motion = move_and_slide(motion, Vector2.UP)
