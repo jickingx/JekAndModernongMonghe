a90dc8b088b0282e021c5785f50fb70610121c89
jickingx <jickingx@gmail.com>
2020-07-09 03:45:54 +0800

    add block that will be destroyed on trigger

---
+++ src/Objects/BlockToBeDestroyedByTrigger.gd
@@ -0,0 +1,17 @@
+extends StaticBody2D
+
+const ParticlesExplosion = preload("res://src/Particles/Explosion.tscn")
+
+func destroy_self():
+	$AudioStreamPlayer.play()
+	var ex = ParticlesExplosion.instance()
+	ex.position = self.position
+	Global.current_scene.add_child(ex)
+	ex.emitting = true
+	
+	yield(get_tree().create_timer(.4), "timeout")
+	queue_free()
+
+
+func _on_Button_pressed():
+	destroy_self()
