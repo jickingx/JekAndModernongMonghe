997af59314d1db530be754f622c5b7c71b33c4ff
Poy Lizardo <aldrinpaullizardo25@gmail.com>
2020-07-10 18:11:15 +0800

    little update: -edit sprite coin in hud and alittle bit of script edit. -import new sprite = mm coins animated sprite. -adding coin sound to jekenkoy player coz i saw its missing. -edit some level objects.

--- src/UI/Hud.gd
+++ src/UI/Hud.gd
@@ -1,13 +1,17 @@
 extends CanvasLayer
 
-const FORMAT_STRING_COINS = " X %s"
+const FORMAT_STRING_COINS = " %s"
 var coins:= 0
 
 func coin_add():
 	var new_text 
 	coins += 1
 	if coins < 10:
+		new_text = "00" + str(coins)
+	elif coins >= 100:
+		new_text = str(coins)
+	elif coins >= 10:
 		new_text = "0" + str(coins)
 	else:
 		new_text = str(coins)
-	$LabelCoins.text = FORMAT_STRING_COINS % new_text
+	$CoinCounter.text = FORMAT_STRING_COINS % new_text
